c7445547ff2ac562ca11adb4677e19b0d5dd5044
Loë159 <loedu44@gmail.com>
2023-06-09 20:32:56 +0200

    Fix bug with death

--- SPW/Constants.h
+++ SPW/Constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Défini la durée de l'animation de la mort du player
-#define PLAYER_DYING_DURATION 60
+#define PLAYER_DYING_DURATION 80
 
 // Défini la gravité par défaut du monde
 #define DEFAULT_WORLD_GRAVITY_Y -40.f
